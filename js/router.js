eccf3dfbcf7c3b06d6e7bfea16dd25d79541c6d0
clientesweb <alexisifran32@gmail.com>
2024-10-02 16:13:25 -0300

    Create router.js

---
+++ js/router.js
@@ -0,0 +1,37 @@
+import { renderHome } from './components/home.js';
+import { renderProperties } from './components/properties.js';
+import { renderContact } from './components/contact.js';
+import { state } from './app.js';
+
+const routes = {
+    '/': renderHome,
+    '/properties': renderProperties,
+    '/contact': renderContact
+};
+
+function initRouter() {
+    window.addEventListener('popstate', handleLocation);
+    document.body.addEventListener('click', (e) => {
+        if (e.target.matches('[data-link]')) {
+            e.preventDefault();
+            navigateTo(e.target.href);
+        }
+    });
+
+    handleLocation();
+}
+
+function handleLocation() {
+    const path = window.location.pathname;
+    const renderFunction = routes[path] || routes['/'];
+    const main = document.getElementById('main');
+    main.innerHTML = '<div class="text-center py-4">Cargando...</div>';
+    renderFunction(main, state.properties);
+}
+
+function navigateTo(url) {
+    history.pushState(null, null, url);
+    handleLocation();
+}
+
+export { initRouter };
