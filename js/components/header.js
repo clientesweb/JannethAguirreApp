96800972e4c092bed7800063ae24cef3ffe7b849
clientesweb <alexisifran32@gmail.com>
2024-10-02 16:49:40 -0300

    Actualizar header.js

--- js/components/header.js
+++ js/components/header.js
@@ -3,13 +3,13 @@ export function renderHeader() {
     header.innerHTML = `
         <div class="container mx-auto px-4 py-2 flex justify-between items-center">
             <div class="flex items-center">
-                <img src="img/logo.png" alt="Janneth Aguirre Bienes Raíces" class="h-12 mr-2">
-                <h1 class="text-xl font-bold">Janneth Aguirre<br>Bienes Raíces</h1>
+                <img src="https://via.placeholder.com/50x50?text=Logo" alt="Janneth Aguirre Bienes Raíces" class="h-12 w-12 mr-2">
+                <h1 class="text-lg md:text-xl font-bold">Janneth Aguirre<br>Bienes Raíces</h1>
             </div>
             <nav class="hidden md:block">
-                <a href="/" class="mx-2 hover:text-yellow-400 transition duration-300" data-link>Inicio</a>
-                <a href="/properties" class="mx-2 hover:text-yellow-400 transition duration-300" data-link>Propiedades</a>
-                <a href="/contact" class="mx-2 hover:text-yellow-400 transition duration-300" data-link>Contacto</a>
+                <a href="/" class="mx-2 hover:text-accent transition duration-300" data-link>Inicio</a>
+                <a href="/properties" class="mx-2 hover:text-accent transition duration-300" data-link>Propiedades</a>
+                <a href="/contact" class="mx-2 hover:text-accent transition duration-300" data-link>Contacto</a>
             </nav>
         </div>
     `;
