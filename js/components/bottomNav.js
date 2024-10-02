473276bf22d6bc288fd6fda1ae67d963005cdf75
clientesweb <alexisifran32@gmail.com>
2024-10-02 17:19:25 -0300

    Actualizar bottomNav.js

--- js/components/bottomNav.js
+++ js/components/bottomNav.js
@@ -1,18 +1,23 @@
 export function renderBottomNav() {
     const bottomNav = document.getElementById('bottom-nav');
     bottomNav.innerHTML = `
-        <div class="flex justify-around items-center h-16">
-            <a href="/" class="bottom-nav-item flex flex-col items-center text-gray-600 hover:text-primary transition duration-300" data-link>
-                <i class="fas fa-home text-xl"></i>
-                <span class="text-xs">Inicio</span>
+        <div class="flex justify-around items-center h-16 bg-white shadow-md fixed bottom-0 w-full">
+            <!-- Inicio -->
+            <a href="/" class="bottom-nav-item flex flex-col items-center text-gray-600 hover:text-blue-600 transition duration-300" data-link>
+                <i class="fas fa-home text-2xl"></i>
+                <span class="text-xs mt-1">Inicio</span>
             </a>
-            <a href="/properties" class="bottom-nav-item flex flex-col items-center text-gray-600 hover:text-primary transition duration-300" data-link>
-                <i class="fas fa-building text-xl"></i>
-                <span class="text-xs">Propiedades</span>
+
+            <!-- Propiedades -->
+            <a href="/properties" class="bottom-nav-item flex flex-col items-center text-gray-600 hover:text-blue-600 transition duration-300" data-link>
+                <i class="fas fa-building text-2xl"></i>
+                <span class="text-xs mt-1">Propiedades</span>
             </a>
-            <a href="/contact" class="bottom-nav-item flex flex-col items-center text-gray-600 hover:text-primary transition duration-300" data-link>
-                <i class="fas fa-envelope text-xl"></i>
-                <span class="text-xs">Contacto</span>
+
+            <!-- Contacto -->
+            <a href="/contact" class="bottom-nav-item flex flex-col items-center text-gray-600 hover:text-blue-600 transition duration-300" data-link>
+                <i class="fas fa-envelope text-2xl"></i>
+                <span class="text-xs mt-1">Contacto</span>
             </a>
         </div>
     `;
