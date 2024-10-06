5732d075519f0b126a2c9fe03961663bb2f7a53f
clientesweb <alexisifran32@gmail.com>
2024-10-06 19:09:42 -0300

    Actualizar service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,25 +1,84 @@
-const CACHE_NAME = 'ja-bienes-raices-v1';
+const CACHE_NAME = 'janneth-aguirre-cache-v1';
 const urlsToCache = [
   '/',
   '/index.html',
   '/styles.css',
   '/script.js',
-  '/images/hero1.jpg',
-  '/images/hero2.jpg',
-  '/images/about-us.jpg',
-  // Add other assets you want to cache
+  '/img/Logo-Janneth-Aguirre-2020-ecuador.png',
+  '/img/hero1.jpg',
+  '/img/hero2.jpg',
+  '/img/hero3.jpg',
+  '/img/property1.jpg',
+  '/img/property2.jpg',
+  '/img/property3.jpg',
+  '/img/property4.jpg',
+  '/img/store1.jpg',
+  '/img/store2.jpg',
+  '/img/store3.jpg',
+  '/img/store4.jpg',
+  '/img/store5.jpg',
+  '/img/store6.jpg',
+  '/img/instagram1.jpg',
+  '/img/instagram2.jpg',
+  '/img/instagram3.jpg',
+  '/img/instagram4.jpg',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
+  'https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600&display=swap',
+  'https://cdn.tailwindcss.com',
+  'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.css',
+  'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick-theme.min.css',
+  'https://code.jquery.com/jquery-3.6.0.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js'
 ];
 
-self.addEventListener('install', event => {
+self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+      .then((cache) => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
   );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => response || fetch(event.request))
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request).then(
+          (response) => {
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            const responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then((cache) => {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
+      })
+    );
+});
+
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
   );
 });
