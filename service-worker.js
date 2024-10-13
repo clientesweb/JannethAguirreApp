3c030d0ad6b65671b0ebc3b557efac7952040cb4
clientesweb <alexisifran32@gmail.com>
2024-10-13 14:11:08 -0300

    Actualizar service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,43 +1,19 @@
-const CACHE_NAME = 'janneth-aguirre-cache-v1';
+const CACHE_NAME = 'ja-bienes-raices-v1';
 const urlsToCache = [
   '/',
   '/index.html',
-  '/styles.css',
   '/script.js',
   '/img/Logo-Janneth-Aguirre-2020-ecuador.png',
-  '/img/hero1.jpg',
-  '/img/hero2.jpg',
-  '/img/hero3.jpg',
-  '/img/property1.jpg',
-  '/img/property2.jpg',
-  '/img/property3.jpg',
-  '/img/property4.jpg',
-  '/img/store1.jpg',
-  '/img/store2.jpg',
-  '/img/store3.jpg',
-  '/img/store4.jpg',
-  '/img/store5.jpg',
-  '/img/store6.jpg',
-  '/img/instagram1.jpg',
-  '/img/instagram2.jpg',
-  '/img/instagram3.jpg',
-  '/img/instagram4.jpg',
-  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
-  'https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600&display=swap',
-  'https://cdn.tailwindcss.com',
-  'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.css',
-  'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick-theme.min.css',
-  'https://code.jquery.com/jquery-3.6.0.min.js',
-  'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js'
+  '/img/favicon-32x32.png',
+  '/img/favicon-16x16.png',
+  '/img/apple-touch-icon.png',
+  '/manifest.json'
 ];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then((cache) => {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
+      .then((cache) => cache.addAll(urlsToCache))
   );
 });
 
@@ -48,24 +24,9 @@ self.addEventListener('fetch', (event) => {
         if (response) {
           return response;
         }
-        return fetch(event.request).then(
-          (response) => {
-            if(!response || response.status !== 200 || response.type !== 'basic') {
-              return response;
-            }
-
-            const responseToCache = response.clone();
-
-            caches.open(CACHE_NAME)
-              .then((cache) => {
-                cache.put(event.request, responseToCache);
-              });
-
-            return response;
-          }
-        );
+        return fetch(event.request);
       })
-    );
+  );
 });
 
 self.addEventListener('activate', (event) => {
