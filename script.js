d38b1c2c1abf240807b410399f463c2f31371486
clientesweb <alexisifran32@gmail.com>
2024-10-15 01:20:13 -0300

    Actualizar script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', function() {
     ];
 
     const heroImages = [
-        "/img/banner.png",
+        "/img/banner1.png",
         "/img/Janneth-Aguirre-bienes-raices-Samborondon-corredora-guayaquil.jpg",
         "/img/Janneth-Aguirre-bienes-raices-constructora-samborondon-1024x514.png"
     ];
