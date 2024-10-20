dc42d6cbf7640ddc071aaa91ee240bf0c43a36b6
clientesweb <alexisifran32@gmail.com>
2024-10-19 23:16:56 -0300

    Create back-to-top.js

---
+++ js/modules/back-to-top.js
@@ -0,0 +1,18 @@
+export function initBackToTopButton() {
+    const backToTopButton = document.getElementById('back-to-top');
+
+    window.addEventListener('scroll', () => {
+        if (window.pageYOffset > 300) {
+            backToTopButton.style.display = 'block';
+        } else {
+            backToTopButton.style.display = 'none';
+        }
+    });
+
+    backToTopButton.addEventListener('click', () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    });
+}
