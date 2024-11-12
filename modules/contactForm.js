7d7ac85794808af67ee4c7a36c06175f0e33e2c4
clientesweb <alexisifran32@gmail.com>
2024-11-12 19:05:15 -0300

    Delete modules directory

--- modules/contactForm.js
+++
@@ -1,8 +0,0 @@
-export function setupContactForm() {
-    document.getElementById('contact-form').addEventListener('submit', function(e) {
-        e.preventDefault();
-        // Implement form validation here
-        alert('Gracias por tu mensaje. Te contactaremos pronto.');
-        this.reset();
-    });
-}
